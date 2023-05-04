56a81fb6058e88ac0a581a1d12258b2bbe77fa66
Kevin Harp <kharp1984@gmail.com>
2023-05-03 20:45:34 -0400

    set up dependencies

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const volleyball = require('volleyball');
+
 const app = express();
+const PORT = 3000;
+
+app.use(volleyball);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
-
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`Listening on port, ${PORT}`)
 });
