fc414a25b15e3e2f37736e4ef36def7e509aca94
Kevin Harp <kharp1984@gmail.com>
2023-05-03 21:11:27 -0400

    added error handler initial

--- app.js
+++ app.js
@@ -41,6 +41,58 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    res.status(404)
+    const html = (/*html*/`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <header><img src="/logo.png"/>Wizard News</header>
+        <div class="not-found">
+          <p>404: Page Not Found</p>
+        </div>
+      </body>
+      </html>
+    `)
+    res.send(html)
+  } else {
+    res.send(/*html*/ `
+      <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Wizard News</title>
+          <link rel="stylesheet" href="/style.css" />
+        </head>
+        <body>
+          <div class="news-list">
+            <header><img src="/logo.png"/>Wizard News</header>
+              <div class='news-item'>
+                <p>
+                  <span class="news-position">${post.id}. ▲</span>
+                  ${post.title}
+                  <small>(by ${post.name})</small>
+                </p>
+                <small class="news-info">
+                  ${post.upvotes} upvotes | ${post.date}
+                </small>
+                <p>
+                  ${post.content}
+                </p>
+              </div>
+          </div>
+        </body>
+      </html>
+    `);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port, ${PORT}`)
 });
