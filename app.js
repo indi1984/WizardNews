33bc0788dd60f7b9d5897233c49b8c0e92844e8d
Kevin Harp <kharp1984@gmail.com>
2023-05-03 21:01:05 -0400

    added new / route html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const volleyball = require('volleyball');
+const postBank = require ('./postBank');
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,36 @@ app.use(volleyball);
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = (/*html*/`
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => /*html*/`
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>
+  `)
+  res.send(html);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port, ${PORT}`)
